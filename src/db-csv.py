64d11a81355d41e32c3522f7d54ac8bbdc753142
Skoh <skookie.omg.lol@gmail.com>
2022-12-22 21:01:47 +0100

    DB rework

---
+++ src/db-csv.py
@@ -0,0 +1,6 @@
+"""
+Methods to fetch data in Google Drive databases
+- Permanent data
+- Static auto actions
+- Shifting auto actions
+"""
---
+++ src/db-gdrive.py
@@ -0,0 +1,4 @@
+"""
+Methods to fetch data in Google Drive databases
+- Memes for daily post
+"""
