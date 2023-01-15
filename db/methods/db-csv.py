72a13b1d6f26637b6195befe106e04f490008a62
Skoh <101289702+SkohTV@users.noreply.github.com>
2023-01-15 19:23:29 +0100

    smol to big

--- db/methods/db-csv.py
+++
@@ -1,6 +0,0 @@
-"""
-Methods to fetch data in Google Drive databases
-- Permanent data
-- Static auto actions
-- Shifting auto actions
-"""
--- db/methods/db-gdrive.py
+++
@@ -1,4 +0,0 @@
-"""
-Methods to fetch data in Google Drive databases
-- Memes for daily post
-"""
