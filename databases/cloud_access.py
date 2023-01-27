6a04c6f754b1c2ce2d9b56a44551afe4c8314ad7
Skoh <101289702+SkohTV@users.noreply.github.com>
2023-01-27 23:25:41 +0100

    MongoDB connection

--- databases/cloud_access.py
+++ databases/cloud_access.py
@@ -18,71 +18,60 @@ from src.data import Login
 
 
 
-def connect() -> MongoClient:
-	"""Create a MongoDB connection"""
-	return MongoClient(Login.MONGO_URL)
 
-def disconnect(client: MongoClient):
-	"""Close the MongoDB connection"""
-	client.close()
+class Atlas:
+	"""Posts from Skoh's Youtube channel"""
 
+	def __init__(self, database, collection) -> None:
+		self.client = self.connect()
+		self.database = self.client[database]
+		self.collection = self.database[collection]
 
+	def connect(self):
+		"""Connect to MongoDB"""
+		return MongoClient(Login.MONGO_URL)
 
-def add(client: MongoClient, database: str, collection: str, data: dict):
-	"""Push data to a MongoDB collection
-
-	Args:
-			client (MongoClient): MongoDB client
-			database (str): Name of the database
-			collection (str): Name of the collection
-			data (dict): Data to push
-	"""
-	pass
+	def disconnect(self):
+		"""Close the MongoDB connection"""
+		self.client.close()
 
 
 
-def supr(client: MongoClient, database: str, collection: str, data: tuple):
-	"""Delete data from a MongoDB collection
+	def add(self, data, name=None):
+		"""Push data to a MongoDB collection"""
+		if name is None:
+			self.collection.insert_one(data)
+		else:
+			self.collection.insert_one({name: data})
 
-	Args:
-			client (MongoClient): MongoDB client
-			database (str): Name of the database
-			collection (str): Name of the collection
-			data (tuple): Data to delete
-	"""
-	pass
-
-
-
-def fetch(client: MongoClient, database: str, collection: str):
-	"""Fetch data from a MongoDB collection
-
-	Args:
-			client (MongoClient): MongoDB client
-			database (str): Name of the database
-			collection (str): Name of the collection
-
-	Returns:
-			dict: Data from the collection
-	"""
-	pass
+	def supr(self, data: dict):
+		"""Delete data from a MongoDB collection"""
+		self.collection.delete_one(data)
 
 
 
-# client = mongoConnect()
+	def query(self, data: dict):
+		"""Query data from a MongoDB collection"""
+		return self.collection.find(data)
 
 
-# database = client["Posts"]
-# collection = database["Youtube-Uploads"]
-# collection.insert_one({"name": "test"})
-
-
-
-# mongoDisconnect(client)
+	def fetch(self):
+		"""Fetch data from a MongoDB collection"""
+		cache_database = []
+		cursor = self.collection.find({})
+		for document in cursor:
+			cache_database.append(document)
+		return cache_database
 
 
 
 
-# cursor = collection.find({})
-# for document in cursor:
-#		 print(document)
+#Temp = Atlas("Posts", "Skoh_Youtube")
+
+#Temp.add({"one" : 1})
+#Temp.add({"two" : 2})
+#Temp.add({"three" : 3})
+
+#Temp.supr(Temp.fetch()[0])
+
+#Temp.disconnect()
--- modules/autopost/youtube_api.py
+++ modules/autopost/youtube_api.py
@@ -1,37 +1,30 @@
 """Connect to Youtube API and make requests"""
 import googleapiclient.discovery
-from pymongo import MongoClient
 from databases import cloud_access as cAccess
 #from src.data import Login, Socials
 from src.logger import logger
+from databases.cloud_access import Atlas
 
 
 
 
 def ytb_connect(api_key: str) -> googleapiclient.discovery.Resource:
-	"""Create a connection to the Youtube API
-
-	Args:
-		API_KEY (str): Given API key located in the .env file
-
-	Returns:
-		googleapiclient.discovery.Resource: Youtube object to use for requests
-	"""
+	"""Create a connection to the Youtube API"""
 	api_service_name, api_version, developer_key = "youtube", "v3", api_key
 	youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey = developer_key)
 	return youtube
 
 
 
-def ytb_request_playlist(client: MongoClient, youtube: googleapiclient.discovery.Resource, playlist_id: str, max_results = 20, create_empty = False):
+def ytb_request_playlist(client: Atlas, youtube: googleapiclient.discovery.Resource, playlist_id: str, max_results = 20, create_empty = False):
 	"""Send a request to Youtube API to fetch videos from a playlist
 
 	Args:
-		client (MongoClient): MongoDB client to connect to the database.
+		client (Atlas): MongoDB client to connect to the database
 		youtube (googleapiclient.discovery.Resource): Initialized Youtube object to connect to Youtube API
 		playlistId (str): The ID of the playlist to fetch
 		maxResults (int, optional): Max number of results to fetch (20 almost every time)
-		create_empty (bool, optional): If True, create a new json file with the channel name as key and an empty list as value. Defaults to False.
+		create_empty (bool, optional): If True, create a new json file with the channel name as key and an empty list as value (defaults to False)
 	"""
 	request = youtube.playlistItems().list(
 		part='snippet',
@@ -39,15 +32,12 @@ def ytb_request_playlist(client: MongoClient, youtube: googleapiclient.discovery
 		playlistId=playlist_id
 	)
 	response = request.execute()
-
-	mongo_ytb = [] if create_empty else cAccess.fetch(client, "Skoh", "Youtube")["videos"]
-	if not create_empty:
-		mongo_ytb.reverse()
+	mongo_ytb = [] if create_empty else client.fetch()
 
 	for item in response["items"]:
 		if not any(d["id"] == item["snippet"]["resourceId"]["videoId"] for d in mongo_ytb):
 			video_info = youtube.videos().list(part='snippet,contentDetails',id=item['snippet']['resourceId']['videoId']).execute()
-			cAccess.add(client, "Posts", "Skoh-Youtube", {
+			client.add({
 				"title": item["snippet"]["title"],
 				"kind": "short" if ("#short" in item["snippet"]["title"]) else ("upcoming" if video_info['items'][0]["snippet"]["liveBroadcastContent"] == "upcoming" else "video"),
 				"posted": "False" if create_empty else "True",
@@ -57,6 +47,5 @@ def ytb_request_playlist(client: MongoClient, youtube: googleapiclient.discovery
 
 	if not create_empty:
 		while len(mongo_ytb) > 20:
-			mongo_ytb.pop(0)
+			client.supr(client.fetch()[0])
 			logger("New video")
-		mongo_ytb.reverse()
