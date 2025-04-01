780a6448eba5b8a4140db703a5471c29dd530e16
SkohTV <qlpth+github@proton.me>
2025-04-01 14:40:39 +0200

    added flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{ outputs = { nixpkgs, ... }:
+
+let
+  allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+  forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+    pkgs = import nixpkgs { inherit system; };
+  });
+
+in {
+  devShells = forAllSystems ({ pkgs }: {
+    default = pkgs.mkShell {
+      packages = with pkgs; [
+        cargo
+        rustc
+        openssl.dev
+      ];
+    };
+
+  });
+};
+}
