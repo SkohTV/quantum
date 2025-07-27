0ff19a934c35bb860a8b3175eb66aa50d7168197
SkohTV <qlpth+github@proton.me>
2025-07-27 18:26:54 +0200

    discord(events): backport

---
+++ src/discord/events/guild_member_addition.rs
@@ -0,0 +1,20 @@
+use serenity::async_trait;
+use poise::serenity_prelude as serenity;
+use crate::discord::{Handler, ids, utils};
+
+
+#[async_trait]
+impl serenity::EventHandler for Handler {
+    async fn guild_member_addition(
+        &self,
+        ctx: serenity::Context,
+        new_member: serenity::Member,
+    ) {
+        let role = serenity::RoleId::from(ids::MEMBER_ROLE);
+        let _ = new_member.add_role(&ctx, role).await;
+
+        let msg = format!("Given role <@&{}> to {}", role, new_member);
+
+        utils::log_to_discord(&ctx, msg, utils::LogRole::Info).await;
+    }
+}
