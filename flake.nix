ea51edba29b48ff90e64d180d3b27ed5b81eb94e
SkohTV <contact@skoh.dev>
2025-11-17 18:21:36 -0500

    nix: refactor flake

--- flake.nix
+++ flake.nix
@@ -1,35 +1,49 @@
 {
 
 inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 };
 
 
 outputs = {
   self,
-  nixpkgs,
-  flake-utils,
+  nixpkgs
 }:
 
+let
+  supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+  forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 
-flake-utils.lib.eachDefaultSystem (system:
+in {
 
-    let
-        pkgs = nixpkgs.legacyPackages.${system};
-        quantum = pkgs.callPackage ./default.nix { };
+  packages = forAllSystems (system: {
+    default = nixpkgsFor.${system}.stdenv.mkDerivation {
+        pname = "quantum";
+        version = "6.x.x";
 
-    in {
-        packages = {
-            inherit quantum;
-            default = quantum;
-        };
+        src = ./.;
 
-        devShells = {
-            default = pkgs.mkShell { inputsFrom = [ quantum ]; };
-        };
-    }
-);
+        nativeBuildInputs = with nixpkgsFor.${system}; [
+          mdbook
+          mdbook-admonish
 
+          cargo
+          rustc
+          rustfmt
+
+          pkg-config
+          openssl.dev
+        ];
+    };
+  });
+
+  devShells = forAllSystems (system: {
+    default = nixpkgsFor.${system}.mkShell {
+      inputsFrom = [ self.packages.${system}.default ];
+    };
+  });
+
+};
 
 }
